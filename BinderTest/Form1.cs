ebb0c2670f470d5522b5037948e083f4f4586ca4
Bob Crowley <bob.crowley@evopayments.com>
2017-10-16 10:01:02 -0400

    Add project files.

---
+++ BinderTest/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BinderTest
+{
+    public partial class Form1 : Form
+    {
+        BindingList<Fruit> lstFruits;
+        public Form1()
+        {
+            InitializeComponent();
+            lstFruits = new BindingList<Fruit>();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = lstFruits;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            lstFruits.Add(new Fruit { FruitName = txtFruitName.Text, FruitColor = txtFruitColor.Text });
+        }
+    }
+}
---
+++ BinderTest/Fruit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinderTest
+{
+    class Fruit
+    {
+        public string FruitName { get; set; }
+        public string FruitColor { get; set; }
+    }
+}
